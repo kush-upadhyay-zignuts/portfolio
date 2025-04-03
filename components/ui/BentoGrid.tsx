2f1958e81cca6cda140bfcec5f9f04a7d972b659
kush-upadhyay-zignuts <kushu@zignuts.com>
2025-04-03 18:12:17 +0530

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -277,22 +277,12 @@ export const BentoGridItem = ({
     },
   };
 
-  // const handleCopy = () => {
-  //   const text = "kushupadhyay1810@gmail.com";
-  //   navigator.clipboard.writeText(text);
-  //   setCopied(true);
-  //    setTimeout(() => setCopied(false), 2000);
-  // };
-
-  const handleCopy = (): void => {
-  const text: string = "kushupadhyay1810@gmail.com";
-  navigator.clipboard.writeText(text).then(() => {
+  const handleCopy = () => {
+    const text = "kushupadhyay1810@gmail.com";
+    navigator.clipboard.writeText(text);
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
-  }).catch((error) => {
-    console.error("Failed to copy text:", error);
-  });
-};
+     
+  };
 
   return (
     <div
